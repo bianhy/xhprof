c11216416b4085b625a0b6d0b2d542991a0a85b5
bianhy <bianhaiyang@songshuai.com>
2019-05-13 15:46:56 +0800

    代码提交

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * Set the absolute paths on your system
+ */
+define('ERROR_FILE', '/tmp/xhprof/logs/xhprof_dot_errfile.log');
+define('TMP_DIRECTORY', '/laragon/xhprof/xhprof_log/');
+define('DOT_BINARY', 'C:\Program Files (x86)\Graphviz2.38\bin\dot.exe');
+
